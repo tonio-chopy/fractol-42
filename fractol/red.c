17709ccd1678aa639755331742195911d57eaff1
Antonio Laualiki <alaualik@3A6.42angouleme.fr>
2025-03-09 00:48:48 +0100

    first part for fractol project.

---
+++ fractol/red.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   red.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaualik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 15:46:13 by alaualik          #+#    #+#             */
+/*   Updated: 2025/03/08 15:46:15 by alaualik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 500, 500, 0x00FF0000);
+	my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
+  mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
+
---
+++ fractol/true_frac/srcs/utils.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaualik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 21:55:01 by alaualik          #+#    #+#             */
+/*   Updated: 2025/03/08 21:55:02 by alaualik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "utils.h"
+
+void  error(char *txt, int mode)
+{
+  if (mode == 0)
+    perror(txt);
+  else if (txt)
+      ft_putstr_fd(txt, 2);
+  exit(EXIT_FAILURE);
+}
+
+void  ft_strlower(char *str)
+{
+  int i;
+
+  if(!str)
+    return ;
+  i = -1;
+  while (str[++i])
+  {
+    if(str[i] >= 'A' && str[i] <= 'Z')
+      str[i] += 32;
+  }
+}
+void  print(char *str, int fd)
+{
+  ft_putstr_fd(str, fd);
+}
+
+void show_usage(void)
+{
+  print("Usage: ./fractol [mandelbrot / julia]\n", 1);
+  exit(EXIT_SUCCESS);
+}
